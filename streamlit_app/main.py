7bb6dc4a29844482f2edd711d69c36e1ad0f8394
Goncharov Anton <adgoncharov_2@edu.hse.ru>
2024-12-30 14:55:13 +0300

    Added logging, changed architecture of the app

--- streamlit_app/main.py
+++ streamlit_app/main.py
@@ -1,10 +1,35 @@
 import requests
+import aiohttp
+import asyncio
 import streamlit as st
-from PIL import Image
 import io
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+from PIL import Image
 
 
-def fetch_adapter_ids():
+def setup_logger(name, log_file, level=logging.INFO):
+    os.makedirs("logs", exist_ok=True)
+
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+    handler = RotatingFileHandler(f"logs/{log_file}", maxBytes=1024 * 1024, backupCount=5)
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+
+def get_models_list():
+    response = requests.get("http://localhost:8000/get_models_list")
+    return response.json()
+
+
+def get_adapter_list():
     response = requests.get("http://localhost:8000/get_adapters_list")
     if response.status_code == 200:
         return response.json()
@@ -13,31 +38,77 @@ def fetch_adapter_ids():
         return []
 
 
+def change_model(new_model):
+    response = requests.post(
+        "http://localhost:8000/change_model",
+        json={"model_type": new_model}
+    )
+    if response.status_code == 200:
+        st.session_state.current_model = new_model
+        return response.json()["message"]
+    else:
+        return f"Error: {response.json()["detail"]}"
+
+
+def change_adapter(new_adapter):
+    response = requests.post(
+        "http://localhost:8000/change_adapter",
+        json={"id": new_adapter}
+    )
+    if response.status_code == 200:
+        st.session_state.current_model = new_adapter
+        return response.json()["message"]
+    else:
+        return f"Error: {response.json()["detail"]}"
+
+
+# Setting up logger to print out logs in "logs/" folder
+logger = setup_logger("streamlit_app", "app.log")
+
 st.title("Anime profile picture generation web app")
+
+logger.info("Application started")
+logger.debug("This is a debug message")
+logger.warning("This is a warning")
 
 # Loading images
 uploaded_images = st.file_uploader("Upload images", type=["png", "jpg", "jpeg"], accept_multiple_files=True)
 
-images = []
-if uploaded_images:
-    for uploaded_image in uploaded_images:
-        image_data = uploaded_image.read()
-        image = Image.open(io.BytesIO(image_data))
-        images.append(image)
-        st.image(image, caption=uploaded_image.name)
+# images = []
+# if uploaded_images:
+#     for uploaded_image in uploaded_images:
+#         image_data = uploaded_image.read()
+#         image = Image.open(io.BytesIO(image_data))
+#         images.append(image)
+#         st.image(image, caption=uploaded_image.name)
 
 # GET responses for CUDA availability, list of models and list of adapters
-cuda_response = requests.get("http://localhost:8000/get_config")
-cuda_available = cuda_response.json().get("device", False)
+# cuda_response = requests.get("http://localhost:8000/get_config")
+# cuda_available = cuda_response.json().get("device", False)
 
-model_response = requests.get("http://localhost:8000/get_models_list")
-model_name = cuda_response.json().get("model_name")
+# model_response = requests.get("http://localhost:8000/get_models_list")
+# model_name = model_response.json().get("id")
 
-adapter_list = fetch_adapter_ids()
+model_list = get_models_list()
+st.session_state.current_model = model_list.json().get("id")[0]
 
-# Radio for choosing the model (standard or anime trained sd1.5)
-model_select = st.radio("Select model", ("standard", "anime"))
-requests.post("http://localhost:8000/change_model", data=model_select)
+selected_model = st.radio(
+    "Select Model Type:",
+    ("standard", "anime"),
+    index=0 if st.session_state.current_model == "anime" else 1
+)
+
+if selected_model != st.session_state.current_model:
+    if st.button("Change Model"):
+        with st.spinner("Changing model..."):
+            result = change_model(selected_model)
+        st.success(result)
+        logger.info(f"Model successfully changed to {selected_model}")
+else:
+    logger.info(f"The {selected_model} model is already selected.")
+
+adapter_list = get_adapter_list()
+st.session_state.current_adapter = model_list.json().get("id")[0]
 
 # Loading custom adapter checkpoint
 uploaded_adapter = st.file_uploader("Upload ip adapter (.bin)", type=["bin"])
@@ -100,18 +171,14 @@ if st.button("Change Adapter"):
 adapter = st.file_uploader('Choose adapter', type=['pth'])
 requests.post("http://localhost:8000/change_adapter", data=adapter)
 
-if cuda_available:
-    comp_method = st.radio("Select computation method:", ('cpu', 'cuda'))
-else:
-    comp_method = 'cpu'
+comp_method = st.radio("Select computation method:", ("cpu", "cuda"))
+if comp_method == "cpu":
+    st.write("Using cpu for computations may be costly. Either reduce the parameters or change to cuda.")
 
-st.write(f'Using: {comp_method} for computations.')
-
-if st.button('Begin anime conversion'):
-    for image in images:
-        if image is not None:
-            result = requests.post("http://localhost:8000/generate_image", data=[image, comp_method])
-            image_path = result.json()
-            gen_image = Image.open(image_path.get('name'))
-            st.image(image, width=500)
-
+# if st.button('Begin anime conversion'):
+#     for image in images:
+#         if image is not None:
+#             result = requests.post("http://localhost:8000/generate_image", data=[image, comp_method])
+#             image_path = result.json()
+#             gen_image = Image.open(image_path.get('name'))
+#             st.image(image, width=500)
